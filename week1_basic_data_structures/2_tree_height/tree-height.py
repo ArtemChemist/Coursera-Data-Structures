ad4d71f3d366e760d14b30f521fbb3fd504242fe
Artem <lebedevfedora@gmail.com>
2021-08-05 16:33:43 +0300

    Build heap

--- week1_basic_data_structures/2_tree_height/tree-height.py
+++ week1_basic_data_structures/2_tree_height/tree-height.py
@@ -4,26 +4,50 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
-class TreeHeight:
+class Tree:
+        nodes = []
         def read(self):
                 self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+                self.input_list = list(map(int, sys.stdin.readline().split()))
+                for ID, ParentID in enumerate(self.input_list):
+                        nd = Node(ID, ParentID, self)
+                        if ParentID == -1:
+                                nd.level = 1
+                        self.nodes.append(nd)
+
+
 
         def compute_height(self):
-                # Replace this code with a faster implementation
                 maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                for nd in self.nodes:
+                        if nd.GetLevel() > maxHeight:
+                                maxHeight = nd.GetLevel()
+                return maxHeight
 
+class Node:
+        def __init__(self, ID, parent_ID, ParentTree:Tree):
+                self.parent = parent_ID
+                self.ID = ID
+                self.level = -1
+                self.Tree = ParentTree
+
+        def GetLevel(self):
+                if self.level >=0:
+                        return self.level
+                else:
+                        try:
+                                self.level = self.Tree.nodes[self.parent].GetLevel()+1
+                        except:
+                                if self.parent == -1:
+                                        self.level = 1
+                        return self.level
+                
 def main():
-  tree = TreeHeight()
+  tree = Tree()
   tree.read()
   print(tree.compute_height())
+
+#if __name__ == "__main__":
+#    main()
 
 threading.Thread(target=main).start()
--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,32 +1,48 @@
 # python3
-
-
-def build_heap(data):
-    """Build a heap from ``data`` inplace.
-
-    Returns a sequence of swaps performed by the algorithm.
-    """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
+class Heap:
+    size = 0
+    data = []
     swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    
+    def __init__(self, s, dt):
+        self.size = s
+        self.data = dt
+
+    def build_heap(self):
+        #Build a heap from ``data`` inplace.
+        #Returns a sequence of swaps performed by the algorithm.
+        i  = len(self.data)//2
+        while (i >= 0):
+            self.SiftDown(i)
+            i-=1
+
+    def SiftDown(self, i):
+        Index = i
+        LeftChildIndex = 2*i+1
+        RightChildIndex = 2*i+2
+        if LeftChildIndex < self.size and self.data[LeftChildIndex] < self.data[Index ]:
+            Index = LeftChildIndex
+        if RightChildIndex < self.size and self.data[RightChildIndex] < self.data[Index ]:
+            Index = RightChildIndex
+        if i != Index:
+            self.Swap (i, Index)
+            self.SiftDown(Index)
+
+    def Swap (self, a, b):
+        self.data[a], self.data[b] = self.data[b], self.data[a]
+        self.swaps.append((a,b))
+
+
+
 
 
 def main():
     n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
-
-    swaps = build_heap(data)
+    hp = Heap(n, data)
+    hp.build_heap()
+    swaps = hp.swaps
 
     print(len(swaps))
     for i, j in swaps:
